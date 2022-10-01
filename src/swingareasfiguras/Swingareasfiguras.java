ba7b2299b4b5cf389317091989e7d33a84d9e01f
user <user@DESKTOP-P4CUQ3D>
2022-10-01 18:57:07 -0500

    Primer commit

---
+++ src/swingareasfiguras/Swingareasfiguras.java
@@ -0,0 +1,14 @@
+package swingareasfiguras;
+
+
+
+public class Swingareasfiguras {
+
+
+    public static void main(String[] args) {
+        Window miventana= new Window();
+        miventana.setVisible(true);
+        
+    }
+    
+}
---
+++ src/swingareasfiguras/Triangulo.java
@@ -0,0 +1,17 @@
+
+package swingareasfiguras;
+
+
+public class Triangulo {
+    double base,altura;
+    
+    public void setBase(double base){
+        this.base=base;
+    }
+    
+    public void setAltura(double altura){
+        this.altura=altura;
+    }
+    
+    
+}
