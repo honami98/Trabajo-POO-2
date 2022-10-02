5a1538e59b930b4d9950bb74319931e9f3d50f1c
user <user@DESKTOP-P4CUQ3D>
2022-10-01 19:00:53 -0500

    Primer commit

---
+++ src/swingecuacion/Swingecuacion.java
@@ -0,0 +1,13 @@
+
+package swingecuacion;
+
+
+public class Swingecuacion {
+
+
+    public static void main(String[] args) {
+        Window miventana= new Window();
+        miventana.setVisible(true);
+    }
+    
+}
